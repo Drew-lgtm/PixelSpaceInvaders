86633e8a606b1af2802864e4a567dc903a94e677
Ondrej Martinek <46805615+OndrejPRG@users.noreply.github.com>
2022-08-29 08:23:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pixel Space Invaders")
 
-# Load images
+# Load images in assets folder
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
